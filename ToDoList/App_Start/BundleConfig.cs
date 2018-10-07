1a0c530389b376b811911e36e01553aac983ea5c
VasylMalanii <vasyl.malanii@gmail.com>
2018-10-07 19:59:43 +0300

    init project

---
+++ ToDoList/App_Start/BundleConfig.cs
@@ -0,0 +1,32 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace ToDoList
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/wwwroot/libs/jquery-{version}.js"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/wwwroot/libs/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/wwwroot/libs/bootstrap.js",
+                      "~/wwwroot/libs/respond.js"));
+
+            bundles.Add(new StyleBundle("~/styles/css").Include(
+                      "~/wwwroot/libs/bootstrap.css",
+                      "~/wwwroot/libs/site.css"));
+
+            // Set EnableOptimizations to false for debugging. For more information,
+            // visit http://go.microsoft.com/fwlink/?LinkId=301862
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
